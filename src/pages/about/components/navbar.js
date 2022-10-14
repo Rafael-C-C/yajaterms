9b1820e384de917102108202ecbf60b0f133e5ad
Rafael Cauich <85263351+Rafael-C-C@users.noreply.github.com>
2022-10-13 22:29:29 -0500

    navbar

---
+++ src/pages/about/components/navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function NavBar() {
+    return (
+        <nav>
+            <h1>YajaDoc</h1>
+            <div className='links'></div>
+        </nav>
+    )
+}
