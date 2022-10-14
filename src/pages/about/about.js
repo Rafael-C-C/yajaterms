096fa372a511a20f017109b15a03fe9345599671
Rafael Cauich <85263351+Rafael-C-C@users.noreply.github.com>
2022-10-13 22:20:39 -0500

    pages added

---
+++ src/pages/about/about.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function About() {
+    return (
+        <div>
+            <h1>Sobre nosotros</h1>
+            <p>Jajsjsjasjajsajsajsajsjaj</p>
+        </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,13 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <section>
+      <div>
+        <h2>Desing</h2>
+        <h3>Develop & hajsa</h3>
+        <p>Diseño de cja</p>
+      </div>
+    </section>
+  )
 }
