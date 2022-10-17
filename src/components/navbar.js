a5fb89950a7f6216ece882485769d2a8d510bc61
Rafael-C-C <xxrafaflooxx@gmai.com>
2022-10-17 13:16:31 -0500

    n

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -8,7 +8,7 @@ export default function NavBar() {
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
 
           <div class="navbar">
-            <a class="active" href="#"><i class="fa fa-fw fa-home"></i>Página principal</a>
+            <a class="active" href="/"><i class="fa fa-fw fa-home"></i>Página principal</a>
             <a href="/about/aviso"><i class="fa fa-fw fa-user"></i> Política de privacidad</a>
             <a href="/about/privacity"><i class="fa fa-address-book"></i> Términos y condiciones</a>
             <a href="/about/contact"><i class="fa fa-fw fa-envelope"></i> Métodos de contaco</a>
